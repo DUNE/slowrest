0afe26ca91cd7568aa93ffd30ab6571cce8b7e72
ligerlac <lino.oscar.gerlach@cern.ch>
2023-04-06 11:14:39 +0200

    prettify queries w/ multiline strings

--- slowrest/queries.py
+++ slowrest/queries.py
@@ -1,19 +1,43 @@
 # get sensor name as function of sensor id
-sensor_name = "SELECT ELEMENT_NAME" \
-                  " from NP04_DCS_01.ELEMENTS" \
-                  " where ELEMENT_ID=:sensor_id"
+sensor_name = '''
+SELECT ELEMENT_NAME
+FROM NP04_DCS_01.ELEMENTS
+WHERE ELEMENT_ID=:sensor_id
+'''
 
 # get [id, name] pairs for all sensors
-sensor_id_name_pairs = "SELECT ELEMENT_ID, ELEMENT_NAME" \
-                       " from NP04_DCS_01.ELEMENTS"
+sensor_id_name_pairs = '''
+SELECT ELEMENT_ID, ELEMENT_NAME
+FROM NP04_DCS_01.ELEMENTS
+'''
 
 # get [timestamp, value] pairs as function of sensor id, from_ts and to_ts
-value_pairs_time_range = "select TS, VALUE_NUMBER" \
-                         " from NP04_DCS_01.VEVENTSCREEN" \
-                         " where ELEMENT_ID=:sensor_id" \
-                         " and TS > :from_ts and TS < :to_ts" \
-                         " order by TS asc"
+value_pairs_time_range = '''
+SELECT TS, VALUE_NUMBER
+FROM NP04_DCS_01.VEVENTSCREEN
+WHERE ELEMENT_ID=:sensor_id
+  AND TS > :from_ts and TS < :to_ts
+ORDER BY TS asc
+'''
 
-# same as above. returns time stamp instead of datetime obj. takes ~20% longer (on DB side). Also, timestamp is rounded differently and two hours off
-value_pairs_time_range_old = "select substr(extract(day from (TS - TIMESTAMP '1970-01-01 00:00:00' AT TIME ZONE 'UTC')) * 24 * 60 * 60 + extract(hour from (TS - TIMESTAMP '1970-01-01 00:00:00' AT TIME ZONE 'UTC')) * 60 * 60 + extract(minute from (TS - TIMESTAMP '1970-01-01 00:00:00' AT TIME ZONE 'UTC')) * 60 + trunc(extract(second from (TS - TIMESTAMP '1970-01-01 00:00:00' AT TIME ZONE 'UTC')),0),0,15)*1000 as TS, VALUE_NUMBER from (select * from NP04_DCS_01.VEVENTSCREEN order by TS asc) where ELEMENT_ID=:sensor_id and TS >= :from_ts and TS <= :to_ts" 
+# same as above. returns time stamp instead of datetime obj. takes ~20% longer (on DB side)
+# Also, timestamp is rounded differently and two hours off (timezone effect)
+value_pairs_time_range_old = '''
+SELECT 
+  SUBSTR(
+    EXTRACT(DAY FROM (TS - TIMESTAMP '1970-01-01 00:00:00' AT TIME ZONE 'UTC')) * 24 * 60 * 60 
+    + EXTRACT(HOUR FROM (TS - TIMESTAMP '1970-01-01 00:00:00' AT TIME ZONE 'UTC')) * 60 * 60 
+    + EXTRACT(MINUTE FROM (TS - TIMESTAMP '1970-01-01 00:00:00' AT TIME ZONE 'UTC')) * 60 
+    + TRUNC(EXTRACT(SECOND FROM (TS - TIMESTAMP '1970-01-01 00:00:00' AT TIME ZONE 'UTC')),0),0,15)*1000 
+    AS TS, 
+  VALUE_NUMBER 
+FROM (
+  SELECT * 
+  FROM NP04_DCS_01.VEVENTSCREEN 
+  WHERE ELEMENT_ID = :sensor_id 
+    AND TS >= :from_ts 
+    AND TS <= :to_ts
+  ORDER BY TS ASC
+)
+'''
 
